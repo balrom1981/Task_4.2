752572172886a36c2c5f956a42583392be509635
balrom1981 <balrom@bk.ru>
2021-02-14 10:03:15 +0300

    Product deleted

--- src/test/java/ru/netology/manager/SearchManagerTest.java
+++ src/test/java/ru/netology/manager/SearchManagerTest.java
@@ -1,8 +1,6 @@
 package ru.netology.manager;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.netology.domain.Product;
 import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
